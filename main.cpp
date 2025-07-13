f5385b4c1dfde2326b219422b18ad49f631576c3
ayush <99374392+ayush-os@users.noreply.github.com>
2025-07-13 16:25:50 -0700

    make my FDE a cycle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 auto main(int argc, char** argv) -> int {
   std::unique_ptr<CPU> atom = std::make_unique<CPU>();
-  atom->decode(atom->fetch(atom->pc));
+  while (true) atom->decode(atom->fetch(atom->pc));
   return 0;
 }
